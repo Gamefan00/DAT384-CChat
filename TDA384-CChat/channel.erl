9b22f667b7fd9ce2d8f62741cd65e3a2fa7652b7
Axel F <nah.forsberg@gmail.com>
2024-02-21 22:41:47 +0100

    Added channel processes

--- TDA384-CChat/channel.erl
+++ TDA384-CChat/channel.erl
@@ -1,4 +1,12 @@
 -module(channel).
--export([start/1,stop/1]).
+%-export([start/1,stop/1]).
 
+start(ChannelAtom) ->
+    
+    gen_server:start(ChannelAtom,initial_state, handle).
 
+    -record(server_st, {
+        cID, % ID of channel
+        channel, % Atom of server
+        uNicks = [] % Current Nicks of active users
+    }).
